51016cfd3dc1ab83c4aaa7d0e7effd863a773e99
PetrosanW <ruslannikolaev5060@gmail.com>
2024-06-17 10:55:13 +0300

    Новые Патчи
    SmokePatch
    AdminPatch

---
+++ AdminPatch/AdminPatch.cs
@@ -0,0 +1,155 @@
+﻿using System.Reflection;
+using Content.Client.Administration.Managers;
+using Content.Client.ContextMenu.UI;
+using Content.Shared.Anomaly.Effects.Components;
+using HarmonyLib;
+using Robust.Shared.GameObjects;
+
+public static class MarseyPatch
+{
+
+    public static string Name = "AdminFlagsPatch";
+
+    public static string Description = "by Petrosan";
+
+    public static bool ignoreFields = true;
+
+}
+
+[HarmonyPatch]
+internal class IsActive
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return (MethodBase)AccessTools.Method(AccessTools.TypeByName("Content.Client.Administration.Managers.ClientAdminManager"), "IsActive");
+    }
+
+    [HarmonyPrefix]
+    private static bool PrefSkip(ref bool __result, ClientAdminManager __instance)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+[HarmonyPatch]
+internal class CanCommand
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return (MethodBase)AccessTools.Method(AccessTools.TypeByName("Content.Client.Administration.Managers.ClientAdminManager"), "CanCommand");
+    }
+
+    [HarmonyPrefix]
+    private static bool PrefSkip(ref bool __result, ClientAdminManager __instance)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+[HarmonyPatch]
+internal class CanAdminPlace
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return (MethodBase)AccessTools.Method(AccessTools.TypeByName("Content.Client.Administration.Managers.ClientAdminManager"), "CanAdminPlace");
+    }
+
+    [HarmonyPrefix]
+    private static bool PrefSkip(ref bool __result, ClientAdminManager __instance)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+[HarmonyPatch]
+internal class CanScript
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return (MethodBase)AccessTools.Method(AccessTools.TypeByName("Content.Client.Administration.Managers.ClientAdminManager"), "CanScript");
+    }
+
+    [HarmonyPrefix]
+    private static bool PrefSkip(ref bool __result, ClientAdminManager __instance)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+[HarmonyPatch]
+internal class CanSay
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return (MethodBase)AccessTools.Method(AccessTools.TypeByName("Content.Client.Administration.Managers.ClientAdminManager"), "CanScript");
+    }
+
+    [HarmonyPrefix]
+    private static bool PrefSkip(ref bool __result, ClientAdminManager __instance)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+[HarmonyPatch]
+internal class CanAdminMenu
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return (MethodBase)AccessTools.Method(AccessTools.TypeByName("Content.Client.Administration.Managers.ClientAdminManager"), "CanAdminMenu");
+    }
+
+    [HarmonyPrefix]
+    private static bool PrefSkip(ref bool __result, ClientAdminManager __instance)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+[HarmonyPatch]
+internal class EntityMenuElementPatch
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return (MethodBase)AccessTools.Method(AccessTools.TypeByName("Content.Client.ContextMenu.UI.EntityMenuElement"), "GetEntityDescription");
+    }
+
+    [HarmonyPostfix]
+    private static void Postfix(EntityUid entity, EntityMenuElement __instance, ref string __result)
+    {
+        object obj = AccessTools.Method(__instance.GetType(), "GetEntityDescriptionAdmin").Invoke((object)__instance, new object[1]
+        {
+            (object) entity
+        });
+        __result = (string)obj;
+    }
+}
+
+[HarmonyPatch]
+static class CommandPermissionPatch
+{
+    [HarmonyTargetMethod]
+    private static MethodBase TargetMethod()
+    {
+        return AccessTools.Method(AccessTools.TypeByName("Robust.Client.Console.ClientConsoleHost"), "CanExecute");
+    }
+
+    [HarmonyPostfix]
+    private static void Postfix(ref bool __result)
+    {
+        __result = true;
+    }
+}
---
+++ SmokePatch/SmokePatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+public static class MarseyPatch
+{
+    public static string Name = "Flash Overlay disabler";
+    public static string Description = "Disables flash overlay.";
+    public static bool ignoreFields = true;
+}
+
+public static class OverlaysPatch
+{
+    private static MethodInfo GetOverlayDraw(string type)
+    {
+        return AccessTools.Method(AccessTools.TypeByName(type), "OnAppearanceChange");
+    }
+
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return GetOverlayDraw("Content.Client.Chemistry.Visualizers.SmokeVisualizerSystem");
+    }
+
+    [HarmonyPrefix]
+    static bool Prefix() => false;
+}
